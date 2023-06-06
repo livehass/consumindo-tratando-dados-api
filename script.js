c05cbd0c887f841c56e9cdb966da846331840fd8
livehass <silva.felipe12@hotmail.com>
2023-06-06 16:47:59 -0300

    adiciona nova função buscaendereço

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-var consultaCep = fetch('https://viacep.com.br/ws/01001250/json/')
-.then( resposta => resposta.json())
-.then( r => { 
-    
-    if(r.erro){
-        throw Error('esse cep não existe')
-    }else
-    
-    console.log(r)})
-.catch(erro => console.log(erro))
-.finally(mensagem => console.log('processamento concluido'));
+async function buscaEndereco(){
 
-console.log(consultaCep)
+var consultaCep = await fetch('https://viacep.com.br/ws/01001250/json/')
+
+}
+
+console.log(consultaCep);
+
+
+
+
+
+
+// .then( resposta => resposta.json())
+// .then( r => { 
+    
+//     if(r.erro){
+//         throw Error('esse cep não existe')
+//     }else
+    
+//     console.log(r)})
+// .catch(erro => console.log(erro))
+// .finally(mensagem => console.log('processamento concluido'));
+
