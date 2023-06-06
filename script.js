a58b1d5c97357613f087ab26077f14f7e64ee3ab
livehass <silva.felipe12@hotmail.com>
2023-06-06 17:04:28 -0300

    git add new if for throw

--- script.js
+++ script.js
@@ -2,28 +2,24 @@ async function buscaEndereco(){
     try {
         var consultaCep = await fetch('https://viacep.com.br/ws/01001000/json/')
         var consultaCepConvertida = await consultaCep.json();
-
+        if (consultaCepConvertida.erro) {
+            throw  Error('Cep não existente')
+        }
         console.log(consultaCepConvertida);
-    } catch (erro) {
-       console.log(erro)    
+    }catch (erro) {
+        console.log(erro)    
     }        
 }
-
 buscaEndereco();
 
-
-
-
-
-
-// .then( resposta => resposta.json())
-// .then( r => { 
+/* .then( resposta => resposta.json())
+ .then( r => { 
     
-//     if(r.erro){
-//         throw Error('esse cep não existe')
-//     }else
+     if(r.erro){
+         throw Error('esse cep não existe')
+     }else
     
-//     console.log(r)})
-// .catch(erro => console.log(erro))
-// .finally(mensagem => console.log('processamento concluido'));
+     console.log(r)})
+ .catch(erro => console.log(erro))
+ .finally(mensagem => console.log('processamento concluido'));*/ 
 
