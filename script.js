7e9eb29d73d8390c3af7ca7ee0a7dab90cf0f949
livehass <silva.felipe12@hotmail.com>
2023-06-06 14:59:11 -0300

    add new if for error

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
 .then( resposta => resposta.json())
-.then( r => console.log(r))
+.then( r => { 
+    
+    if(r.erro){
+        throw error('esse cep não existe')
+    }else
+    
+    console.log(r)})
 .catch(erro => console.log(erro));
 
 console.log(consultaCep)
