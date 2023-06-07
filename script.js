86d3ff67931cc536c0d684ea1c541f456cf2bf22
livehass <silva.felipe12@hotmail.com>
2023-06-06 21:58:28 -0300

    add new function

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 async function buscaEndereco(cep){
     try {
-        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json`);
         var consultaCepConvertida = await consultaCep.json();
         if (consultaCepConvertida.erro) {
-            throw  Error('Cep não existente')
+            throw Error('Cep não existente!');
         }
         console.log(consultaCepConvertida);
         return consultaCepConvertida;
-    }catch (erro) {
+    } catch (erro) {
         console.log(erro)   
     }        
 }
-let ceps = ['01001000', '01001001']
-let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+// 
+
+
 buscaEndereco();
 
-Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
 
+// let ceps = ['01001000', '01001001'];
+// let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+// Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
 
-console.log('teste', conjuntoCeps)
 /* .then( resposta => resposta.json())
  .then( r => { 
     
